e6913eecfa71045d05b29a7bb44208479d049795
Richard Bizik <richard.bizik@gmail.com>
2022-09-20 16:24:38 +0200

    polybar: add sink switch

---
+++ .config/polybar/scripts/sink-switch.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+speakers="alsa_output.pci-0000_0e_00.4.analog-stereo"
+headphones="alsa_output.usb-SteelSeries_Arctis_Pro_Wireless-00.stereo-game"
+arg="${1:-}"
+
+activate_sink () {
+	SINK="$1"
+	pacmd set-default-sink "$SINK"
+	pacmd list-sink-inputs | grep index | while read line; do
+		pacmd move-sink-input `echo $line | cut -f2 -d' '` "$SINK"
+	done
+}
+
+case "$arg" in
+  --speakers)
+		activate_sink "$speakers"
+    notify-send "Switched to speakers!"
+		echo ""
+    ;;
+  --headphones)
+		activate_sink "$headphones"
+    notify-send "Switched to headphones!"
+		echo ""
+    ;;
+  --toggle)
+		current=$(pactl get-default-sink)
+		case "$current" in
+			$speakers)
+				activate_sink "$headphones"
+				notify-send "Switched to headphones!"
+				echo ""
+			  ;;
+			$headphones)
+				activate_sink "$speakers"
+				notify-send "Switched to speakers!"
+				echo ""
+				;;
+		esac
+    ;;
+  *)
+		current=$(pactl get-default-sink)
+		case "$current" in
+			$speakers)
+				echo ""
+			  ;;
+			$headphones)
+				echo ""
+				;;
+		  *)
+				echo "$current"
+		esac
+    ;;
+esac
